6fbd46ee332e7e32b76046fac7215379e21f25a0
Sijongokuhle Jikijela <ST10374043@rcconnect.edu.za>
2024-10-24 16:17:51 +0000

    Add project files.

---
+++ Models/Lecturer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMC_SYSTEM.Models
+{
+    public class Lecturer
+    {
+        public int LecturerId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
